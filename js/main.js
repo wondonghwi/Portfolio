505a8f247d2db88e5496315dcba7342d6ff02b31
donghwi <ehdehd1289@naver.com>
2020-12-03 03:09:43 +0900

    js : remove active from nav

--- js/main.js
+++ js/main.js
@@ -61,6 +61,15 @@ workBtnContainer.addEventListener('click', (e) => {
   if (!filter) {
     return;
   }
+
+  //Remove selection and activity
+  const active = document.querySelector('.category__btn.selected');
+  active.classList.remove('selected');
+  const target = e.target.nodeName === 'BUTTON'
+    ? e.target
+    : e.target.parentNode
+  target.classList.add('selected');
+
   // console.log(filter)
   projectContainer.classList.add('anim-out');
   setTimeout(() => {
@@ -74,7 +83,6 @@ workBtnContainer.addEventListener('click', (e) => {
     projectContainer.classList.remove('anim-out');
   }, 300);
 });
-
 
 
 //Common Function
