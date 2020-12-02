d264c225711e44251db1dece98d20a182dd5e7b2
donghwi <ehdehd1289@naver.com>
2020-12-02 23:08:52 +0900

    js : navbar menu click

--- js/main.js
+++ js/main.js
@@ -3,10 +3,24 @@
 //Make navbar the top
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
-document.addEventListener('scroll' , () => {
+document.addEventListener('scroll', () => {
   if (window.scrollY > navbarHeight) {
     navbar.classList.add('navbar--dark');
   } else {
     navbar.classList.remove('navbar--dark');
   }
-})
+});
+
+//Handle Scrolling
+const navMenu = document.querySelector('.nav__menu');
+navMenu.addEventListener('click', (e) => {
+  const target = e.target;
+  const link = target.dataset.link;
+  if (!link) {
+    return;
+  }
+  console.log(link);
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({behavior: "smooth"})
+});
+
