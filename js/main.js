5964eb2921b5dd087e9045fb0322f4e344301576
donghwi <ehdehd1289@naver.com>
2020-12-04 11:36:48 +0900

    js : handle Scroll ++

--- js/main.js
+++ js/main.js
@@ -20,8 +20,8 @@ navMenu.addEventListener('click', (e) => {
     return;
   }
   navMenu.classList.remove('open');
-  // console.log(link);
   scrollIntoView(link);
+  selectNavItem(target);
 });
 
 // Navbar toggle btn
@@ -95,5 +95,50 @@ workBtnContainer.addEventListener('click', (e) => {
 //Common Function
 const scrollIntoView = (selector) => {
   const scrollTo = document.querySelector(selector);
-  scrollTo.scrollIntoView({behavior: "smooth"})
+  scrollTo.scrollIntoView({behavior: "smooth"});
 };
+
+//Scroll관련 효과
+const sectionIds = [
+  '#home',
+  '#about',
+  '#skills',
+  '#work',
+  '#contact',
+];
+const sections = sectionIds.map(id => document.querySelector(id));
+const navItems = sectionIds.map(id =>
+  document.querySelector(`[data-link="${id}"]`)
+);
+
+let selectedNavIndex = 0;
+let selectedNavItem = navItems[0];
+function selectNavItem(selected) {
+  selectedNavItem.classList.remove('active');
+  selectedNavItem = selected;
+  selectedNavItem.classList.add('active');
+}
+
+const observerOptions = {
+  root: null,
+  rootMargin: '0px',
+  threshold: 0.3,
+};
+
+const observerCallback = (entries, observer) => {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting && entry.intersectionRatio > 0) {
+      const index = sectionIds.indexOf(`#${entry.target.id}`);
+      // 스크롤링이 아래로 되어서 페이지가 올라옴
+      if (entry.boundingClientRect.y < 0) {
+        selectedNavIndex = index + 1;
+      } else {
+        selectedNavIndex = index - 1;
+      }
+    }
+  });
+};
+
+const observer = new IntersectionObserver(observerCallback, observerOptions);
+sections.forEach(section => observer.observe(section));
+
